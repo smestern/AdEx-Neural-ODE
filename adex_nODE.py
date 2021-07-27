Fix event driven gradient

adex_nODE.py
@@ -69,7 +69,6 @@ class AdEx(nn.Module):
         return dvdt, dwdt
 
     def event_fn(self, t, state):
-        
         V, w = state
         return -(V - self.V_thres)
 
@@ -93,7 +92,7 @@ class AdEx(nn.Module):
             #This ensures we get finding events (spikes) until there are not more, but will likely break autograd
             # come up with a way to fix it?
             try:
-                event_t, solution = odeint_event(self, state, t0, event_fn=self.event_fn, reverse_time=False, atol=1e-8, rtol=1e-8, odeint_interface=self.odeint, method='euler', options={'step_size':self.step_size})
+                event_t, solution = odeint_event(self, state, t0, event_fn=self.event_fn, reverse_time=False, atol=5e-5, rtol=5e-5, odeint_interface=self.odeint, method='euler', options={'step_size':self.step_size})
                 event_times.append(event_t)
 
                 state = self.state_update(tuple(s[-1] for s in solution))
@@ -119,9 +118,9 @@ class AdEx(nn.Module):
         adapt = [state[1][None]]
         times = [t0.reshape(-1)]
         for event_t in event_times:
-            tt = torch.linspace(float(t0), float(event_t), int((float(event_t) - float(t0)) * (1/self.step_size)))[1:-1]
+            tt = torch.linspace(float(t0), float(event_t), int((float(event_t) - float(t0)) * (1/self.step_size) + 1))[1:-1]
             tt = torch.cat([t0.reshape(-1), tt, event_t.reshape(-1)])
-            solution = odeint(self, state, tt, atol=1e-8, rtol=1e-8, method='euler', options={'step_size':self.step_size})
+            solution = odeint(self, state, tt, atol=5e-5, rtol=5e-5, method='euler', options={'step_size':self.step_size, 'perturb': True})
 
             voltage.append(solution[0])
             adapt.append(solution[1])
@@ -130,4 +129,4 @@ class AdEx(nn.Module):
             state = self.state_update(tuple(s[-1] for s in solution))
             t0 = event_t
 
-        return torch.cat(times), torch.cat(voltage, dim=0).reshape(-1), torch.cat(adapt, dim=0).reshape(-1), event_times
+        return torch.cat(times), torch.cat(voltage, dim=0).reshape(-1), torch.cat(adapt, dim=0).reshape(-1), torch.stack(event_times)

example3_event_driven.py
@@ -10,51 +10,74 @@ from torchdiffeq import odeint_event
 
 torch.set_default_dtype(torch.float64)
 
-
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
 
 if __name__ == "__main__":
 
+    with torch.no_grad():
+        res = torch.as_tensor(np.load("example.npy")/ 1000) 
+        res_spikes = torch.as_tensor(np.load("example_spikes.npy"))
 
-
-    system = AdEx(V_rest=-0.060, event_driven=True)
+    system = AdEx(V_rest=-0.060, event_driven=True, adjoint=True)
     times, voltage, adapt, event_times = system.simulate()
 
-    #system = AdEx(V_rest=-0.065, event_driven=True)
-    #times, voltage2, adapt2, event_times = system.simulate()
+    system2 = AdEx(V_rest=-0.065, event_driven=True).to(device)
+    times2, voltage2, adapt2, event_times2 = system2.simulate()
 
-    #loss = (voltage - voltage2).sum()
-    #loss.backward()
+    loss = nn.MSELoss()
 
-    times = times.detach().cpu().numpy()
-    voltage = voltage.detach().cpu().numpy() * 1000
-    adapt = adapt.detach().cpu().numpy() * 1000
-    
+    optim = torch.optim.Adam([{
+        'params': [system.b, system.a, system.w_intial], 'lr': 1e-13},
+        {'params': [system.R], 'lr': 1e7}, #Carefully define the learning rate for each parameter. 
+        #Otherwise too high LR with explode the gradient, too low makes no difference on the param
+        {'params': [system.V_rest, system.V_reset, system.V_T, system.delta_T, system.V_intial, system.tau, system.tau_w]}], lr=1e-4)
+        
+    with torch.no_grad():     
+            print(system.__dict__)
+    for x in np.arange(500):
+        optim.zero_grad()
+        times, voltage, adapt, event_times = system.simulate()
+        out = loss(event_times[0], event_times2[0]) * 1000
+        out += loss(event_times[-1], event_times2[-1]) * 1000
+        out += loss(voltage[:40000], voltage2[:40000])
+        out.backward(retain_graph=True)
+        optim.step() #gradient descent
+        print("==== loss ====")
+        print(out)
+        with torch.no_grad():     
+            print(system.__dict__)
+        #loss = (voltage - voltage2).sum()
+            times_ = times.detach().cpu().numpy()
+            times_2 = times2.detach().cpu().numpy()
+            plt.clf()
+            volt, = plt.plot(times_, voltage.detach().cpu().numpy() * 1000, color="C0", linewidth=2.0)
+            volt2, = plt.plot(times_2, voltage2.detach().cpu().numpy()*1000, color="r", linewidth=2.0)
+            fspikes = np.ravel(event_times.detach().cpu().numpy())
+            plt.scatter(fspikes, np.full(fspikes.shape[0], 0))
+            plt.xlim([times[0], times[-1]])
+            plt.ylim([-100, 20])
+            plt.ylabel("Membrane Voltage (mV)", fontsize=16)
+            plt.xlabel("Time", fontsize=13)
+            plt.legend([volt, volt2], ["Real Trace", "Fit"], fontsize=16)
 
-    plt.figure(figsize=(7, 3.5))
+            plt.gca().xaxis.set_tick_params(direction='in', which='both')  # The bottom will maintain the default of 'out'
+            plt.gca().yaxis.set_tick_params(direction='in', which='both')  # The bottom will maintain the default of 'out'
 
-    
+                # Hide the right and top spines
+            plt.gca().spines['right'].set_visible(False)
+            plt.gca().spines['top'].set_visible(False)
 
-    volt2, = plt.plot(times, adapt, color="C1", alpha=0.7, linestyle="--", linewidth=2.0)
-    volt, = plt.plot(times, voltage, color="C0", linewidth=2.0)
+                # Only show ticks on the left and bottom spines
+            plt.gca().yaxis.set_ticks_position('left')
+            plt.gca().xaxis.set_ticks_position('bottom')
 
-    plt.hlines(0, 0, 100)
-    plt.xlim([times[0], times[-1]])
-    plt.ylim([voltage.min() - 20, 20])
-    plt.ylabel("Membrane Voltage (mV)", fontsize=16)
-    plt.xlabel("Time", fontsize=13)
-    plt.legend([volt, volt2], ["Fit 1", "adapt"], fontsize=16)
+            plt.tight_layout()
+                #plt.pause(0.05)
+            plt.savefig(f"output/{x}_fit.png")
 
-    plt.gca().xaxis.set_tick_params(direction='in', which='both')  # The bottom will maintain the default of 'out'
-    plt.gca().yaxis.set_tick_params(direction='in', which='both')  # The bottom will maintain the default of 'out'
 
-    # Hide the right and top spines
-    plt.gca().spines['right'].set_visible(False)
-    plt.gca().spines['top'].set_visible(False)
 
-    # Only show ticks on the left and bottom spines
-    plt.gca().yaxis.set_ticks_position('left')
-    plt.gca().xaxis.set_ticks_position('bottom')
 
-    plt.tight_layout()
-    plt.show()
-    plt.savefig("bouncing_ball.png")